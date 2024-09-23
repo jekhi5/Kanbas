0fd6335a92c360c419908894da620481060a6686
Jacob Kline <jacobk513@gmail.com>
2024-09-22 21:30:19 -0400

    finished assignments page

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -3,6 +3,7 @@ import CoursesNavigation from "./Navigation";
 import { useParams } from 'react-router-dom';
 import Modules from "./Modules";
 import Home from "./Home";
+import Assignments from "./Assignments";
 
 export default function Courses() {
     const { cid } = useParams();
@@ -20,7 +21,7 @@ export default function Courses() {
                             <Route path="/" element={<Navigate to="Home" />} />
                             <Route path="Home" element={<Home />} />
                             <Route path="Modules" element={<Modules />} />
-                            <Route path="Assignments" element={<h2>Assignments</h2>} />
+                            <Route path="Assignments" element={<Assignments />} />
                             <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>} />
                             <Route path="People" element={<h2>People</h2>} />
                         </Routes>
