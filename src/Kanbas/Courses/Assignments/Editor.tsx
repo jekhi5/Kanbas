f7b12df67b026170635167d84d64148419b8f31b
Jacob Kline <jacobk513@gmail.com>
2024-09-23 11:04:32 -0400

    finished assignment editor

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -3,9 +3,10 @@ export default function AssignmentEditor() {
         <div id="wd-assignments-editor">
             <label htmlFor="wd-name"><h3>Assignment Name</h3></label>
             <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
-            <textarea id="wd-description">
+            <textarea id="wd-description" cols={50} rows={8}>
                 The assignment is available online Submit a link to the landing page of your Web application running on Netlify. The landing page should include the following: Your full name and section Links to each of the lab assignments Link to the Kanbas application Links to all relevant source code repositories The Kanbas application should include a link to navigate back to the landing page.
             </textarea>
+            <br />
             <br />
             <table>
                 <tr>
@@ -58,46 +59,41 @@ export default function AssignmentEditor() {
                 </tr>
                 <br />
                 <tr>
-                    <td align="right" valign="top">
+                    <td></td>
+                    <td align="left" valign="top">
                         Online Entry Option
                     </td>
                 </tr>
                 <tr>
-                    <td align="right">
-                        <td align="left">
-                            <div>
-                                <input id="wd-text-entry" type="checkbox" />
-                                <label htmlFor="wd-text-entry">Text Entry</label>
-                                <br />
-                                <input id="wd-website-url" type="checkbox" />
-                                <label htmlFor="wd-website-url">Website URL</label>
-                                <br />
-                                <input id="wd-media-recordings" type="checkbox" />
-                                <label htmlFor="wd-media-recordings">Media Recordings</label>
-                                <br />
-                                <input id="wd-student-annotation" type="checkbox" />
-                                <label htmlFor="wd-student-annotation">Student Annotation</label>
-                                <br />
-                                <input id="wd-file-upload" type="checkbox" />
-                                <label htmlFor="wd-file-upload">File Uploads</label>
-                            </div>
-                        </td>
+                    <td></td>
+                    <td align="left">
+                        <input id="wd-text-entry" type="checkbox" />
+                        <label htmlFor="wd-text-entry">Text Entry</label>
+                        <br />
+                        <input id="wd-website-url" type="checkbox" />
+                        <label htmlFor="wd-website-url">Website URL</label>
+                        <br />
+                        <input id="wd-media-recordings" type="checkbox" />
+                        <label htmlFor="wd-media-recordings">Media Recordings</label>
+                        <br />
+                        <input id="wd-student-annotation" type="checkbox" />
+                        <label htmlFor="wd-student-annotation">Student Annotation</label>
+                        <br />
+                        <input id="wd-file-upload" type="checkbox" />
+                        <label htmlFor="wd-file-upload">File Uploads</label>
                     </td>
                 </tr>
                 <br />
                 <tr>
                     <td align="right" valign="top">
-                        <label htmlFor="wd-assign-to">Assign to</label>
+                        <label>Assign </label>
                     </td>
-                </tr>
-                <tr>
-                    <td align="center" valign="top">
-                        <input id="wd-assign-to" type="text" value="Everyone" />
-                    </td>
-                </tr>
-                <br />
-                <tr>
                     <td>
+                        <label htmlFor="wd-assign-to">Assign to</label>
+                        <br />
+                        <input id="wd-assign-to" type="text" value="Everyone" />
+                        <br />
+                        <br />
                         <label htmlFor="wd-due-date">Due</label>
                         <br />
                         <input id="wd-due-date" type="date" value="2024-05-13" />
@@ -105,6 +101,7 @@ export default function AssignmentEditor() {
                 </tr>
                 <br />
                 <tr>
+                    <td></td>
                     <td>
                         <label htmlFor="wd-available-from">Available from</label>
                         <br />
@@ -117,13 +114,13 @@ export default function AssignmentEditor() {
                     </td>
                 </tr>
                 <tr>
-                    <td>
+                    <td colSpan={3}>
                         <hr />
                     </td>
                 </tr>
                 <tr>
-                    <td align="right">
-                        <button >Cancel</button>
+                    <td colSpan={3} align="right">
+                        <button>Cancel</button>
                         <button>Save</button>
                     </td>
                 </tr>
