4abf8a6a5f14fb9bd4ed8455cefab827337c3788
Jacob Kline <jacobk513@gmail.com>
2024-10-08 18:23:49 -0400

    finished labs and assignment viewing page

--- src/Kanbas/CourseTiles/CS3000.tsx
+++ src/Kanbas/CourseTiles/CS3000.tsx
@@ -2,20 +2,22 @@ import { Link } from "react-router-dom";
 
 export default function CS3000() {
     return (
-        <div className="wd-dashboard-course">
-            <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/3000/Home">
-                <img src={require("../../public/images/OOD.jpg")} alt="react logo" width={200} />
-                <div>
-                    <h5>
-                        CS3000
-                    </h5>
-                    <p className="wd-dashboard-course-title">
-                        Object Oriented Design
-                    </p>
-                    <button> Go </button>
-                </div>
-            </Link>
+        <div className="wd-dashboard-course col" style={{ width: "260px" }}>
+            <div className="card rounded-3 overflow-hidden">
+                <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/3000/Home">
+                    <img src={require("../../public/images/OOD.jpg")} alt="react logo" width="100%" height={160} />
+                    <div className="card-body">
+                        <h5 className="wd-dashboard-course-title card-title">
+                            CS3000
+                        </h5>
+                        <p className="wd-dashboard-course-title card-text">
+                            Object Oriented Design
+                        </p>
+                        <button className="btn btn-primary"> Go </button>
+                    </div>
+                </Link>
+            </div>
         </div>
     );
 }
