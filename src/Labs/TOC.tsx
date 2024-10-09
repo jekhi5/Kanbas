c105d5f3f870751d71dbcf7b991463ea6ede6ea4
Jacob Kline <jacobk513@gmail.com>
2024-10-08 23:36:27 -0400

    fixed github link

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -32,7 +32,7 @@ export default function TOC() {
                 </a>
             </li>
             <li className="nav-item">
-                <a id="wd-k" href="https://github.com/jannunzi" className="nav-link">
+                <a id="wd-github" href="https://github.com/jekhi5/kanbas-react-web-app-cs4550-f24/" className="nav-link">
                     My GitHub
                 </a>
             </li>
