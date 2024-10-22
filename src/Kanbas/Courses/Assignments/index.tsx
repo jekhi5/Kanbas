ae528bd4a89036a4afb55ab4bdd57c80ba4eb402
Jacob Kline <jacobk513@gmail.com>
2024-10-21 23:28:17 -0400

    finished through assignment page

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -4,9 +4,17 @@ import AssignmentControlButtons from "./AssignmentControlButton";
 import AssignmentHeadingControlButtons from "./AssignmentHeadingControlButtons";
 import { PiNotePencilDuotone } from "react-icons/pi";
 import { IoIosArrowDown } from "react-icons/io";
+import * as db from "../../Database";
+import { useParams } from "react-router";
+import { format } from 'date-fns';
 
 
 export default function Assignments() {
+    const { cid } = useParams();
+    const assignments = db.default.assignments;
+
+    const todaysDate = new Date().setHours(0, 0, 0, 0);
+
     return (
         <div>
             <AssignmentsControls />
@@ -22,66 +30,29 @@ export default function Assignments() {
                         </div>
                     </div>
                     <ul className="wd-lessons list-group rounded-0">
-                        <li className="wd-assignment-list-item list-group-item p-3 ps-1 py-0">
-                            <div className="d-flex mb-3">
-                                <div className="p-2 my-auto">
-                                    <BsGripVertical className="me-2 fs-3" />
-                                    <PiNotePencilDuotone className="me-2 fs-5" />
-                                </div>
-                                <div className="p-2 my-auto">
-                                    <h3>
-                                        <a className="wd-assignment-link text-dark text-decoration-none"
-                                            href="#/Kanbas/Courses/1234/Assignments/123">
-                                            A1 - ENV + HTML
-                                        </a>
-                                    </h3>
-                                    <span className="text-danger"> Multiple Modules </span> | <span><b>Not Available until</b> May 6 at 12:00am</span> | <span><b>Due</b> May 13 at 11:59pm | 100 pts</span>
-                                </div>
-                                <div className="ms-auto p-2 my-auto">
-                                    <AssignmentControlButtons />
-                                </div>
-                            </div>
-                        </li>
-                        <li className="wd-assignment-list-item list-group-item p-3 ps-1 py-0">
-                            <div className="d-flex mb-3">
-                                <div className="p-2 my-auto">
-                                    <BsGripVertical className="me-2 fs-3" />
-                                    <PiNotePencilDuotone className="me-2 fs-5" />
-                                </div>
-                                <div className="p-2 my-auto">
-                                    <h3>
-                                        <a className="wd-assignment-link text-dark text-decoration-none"
-                                            href="#/Kanbas/Courses/1234/Assignments/123">
-                                            A2 - CSS + BOOTSTRAP
-                                        </a>
-                                    </h3>
-                                    <span className="text-danger"> Multiple Modules </span>| <span><b>Not Available until</b> May 13 at 12:00am</span> | <span><b>Due</b> May 20 at 11:59pm | 100 pts</span>
-                                </div>
-                                <div className="ms-auto p-2 my-auto">
-                                    <AssignmentControlButtons />
-                                </div>
-                            </div>
-                        </li>
-                        <li className="wd-assignment-list-item list-group-item p-3 ps-1 py-0">
-                            <div className="d-flex mb-3">
-                                <div className="p-2 my-auto">
-                                    <BsGripVertical className="me-2 fs-3" />
-                                    <PiNotePencilDuotone className="me-2 fs-5" />
-                                </div>
-                                <div className="p-2 my-auto">
-                                    <h3>
-                                        <a className="wd-assignment-link text-dark text-decoration-none"
-                                            href="#/Kanbas/Courses/1234/Assignments/123">
-                                            A3 - JAVASCRIPT + REACT
-                                        </a>
-                                    </h3>
-                                    <span className="text-danger">Multiple Modules</span> | <span><b>Not Available until</b> May 20 at 12:00am</span> | <span><b>Due</b> May 27 at 11:59pm | 100 pts</span>
-                                </div>
-                                <div className="ms-auto p-2 my-auto">
-                                    <AssignmentControlButtons />
-                                </div>
-                            </div>
-                        </li>
+                        {assignments
+                            .filter((assignment: any) => assignment.course === cid)
+                            .map((assignment: any) => (
+                                <li className="wd-assignment-list-item list-group-item p-3 ps-1 py-0">
+                                    <div className="d-flex mb-3">
+                                        <div className="p-2 my-auto">
+                                            <BsGripVertical className="me-2 fs-3" />
+                                            <PiNotePencilDuotone className="me-2 fs-5" />
+                                        </div>
+                                        <div className="p-2 my-auto">
+                                            <h3>
+                                                <a className="wd-assignment-link text-dark text-decoration-none"
+                                                    href={`#/Kanbas/Courses/${cid}/Assignments/${assignment._id}`}>
+                                                    {assignment.title}
+                                                </a>
+                                            </h3>
+                                            <span className="text-danger"> Multiple Modules </span> | {(Date.parse(assignment.releaseDate.replace(/-/g, " ")) > todaysDate) ? <span><b>Not Available until </b>{format(assignment.releaseDate, "MMMM d 'at' hh:mma") + ' |'}</span> : ''} <span><b>Due</b> {format(assignment.dueDate, "MMMM d 'at' hh:mma")} | {assignment.points} pts</span>
+                                        </div>
+                                        <div className="ms-auto p-2 my-auto">
+                                            <AssignmentControlButtons />
+                                        </div>
+                                    </div>
+                                </li>))}
                     </ul>
                 </li>
             </ul>
