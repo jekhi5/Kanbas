d71f1fcdc18df47772c4135780419cc7a2d34ee7
Jacob Kline <jacobk513@gmail.com>
2024-10-21 23:51:08 -0400

    finished assignment 3

--- src/Kanbas/Database/index.ts
+++ src/Kanbas/Database/index.ts
@@ -1,9 +1,13 @@
 import courses from './courses.json';
 import modules from './modules.json';
 import assignments from './assignments.json';
+import users from './users.json';
+import enrollments from './enrollments.json';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   courses,
   modules,
   assignments,
+  users,
+  enrollments,
 };
