1e8de22ae822bd4fdcbe4312e0913c57ea8e692e
Jacob Kline <jacobk513@gmail.com>
2024-11-14 17:35:45 -0500

    quiz editor - details + new question

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -4,19 +4,30 @@ import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
+import QuizEditor from "./Quizzes/Editor";
 import { FaAlignJustify } from "react-icons/fa";
 import PeopleTable from "./People/Table";
+import * as db from '../Database';
 
 export default function Courses({ courses }: { courses: any[]; }) {
     const { cid } = useParams();
     const { pathname } = useLocation();
     const course = courses.find((course) => course._id === cid);
+    const quizzes = db.quizzes;
+    const quizId = pathname.split("/")[5];
+    const quiz = quizzes.find(quiz => quiz._id === quizId);
 
     return (
         <div id="wd-courses">
             <h2 className="text-danger">
                 <FaAlignJustify className="me-4 fs-4 mb-1" />
                 {course && course.name} &gt; {pathname.split("/")[4]}
+                {quiz && quiz.title ? (
+                    <>
+                        <span className="text-danger"> {' > '} </span>
+                        <span className="text-secondary">{quiz.title}</span>
+                    </>
+                ) : null}
             </h2>
             <hr />
             <div className="d-flex">
@@ -31,6 +42,7 @@ export default function Courses({ courses }: { courses: any[]; }) {
                         <Route path="Modules" element={<Modules />} />
                         <Route path="Assignments" element={<Assignments />} />
                         <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+                        <Route path="Quizzes/:qid" element={<QuizEditor />} />
                         <Route path="Assignments/new" element={<AssignmentEditor />} />
                         <Route path="People" element={<PeopleTable />} />
                     </Routes>
