dcc9bf633097d4c6d04cfb4dcff9b247922b01e9
Jacob Kline <jacobk513@gmail.com>
2024-11-22 18:34:23 -0500

    pull server out of repo

--- server/Kanbas/Enrollments/routes.js
+++
@@ -1,8 +0,0 @@
-import * as dao from './dao.js';
-
-export default function EnrollmentRoutes(app) {
-    app.get('/api/enrollments', (req, res) => {
-        const enrollments = dao.fetchAllEnrollments();
-        res.send(enrollments);
-    });
-}
