4656be250b6deaf6f1db0b6b56bb690a0128ecf1
Jacob Kline <jacobk513@gmail.com>
2024-11-22 16:36:31 -0500

    user reload bug actually fixed now

--- client/src/Kanbas/Courses/ProtectedCourse.tsx
+++ client/src/Kanbas/Courses/ProtectedCourse.tsx
@@ -4,15 +4,19 @@ export default function ProtectedCourse({ children }: { children: any }) {
   const { cid } = useParams();
   const { enrollments } = useSelector((state: any) => state.enrollmentsReducer);
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  return currentUser.role === 'FACULTY' ||
+  if (!currentUser) {
+    return <Navigate to="/Kanbas/Account/Signin" />;
+  } else if (
+    currentUser.role === 'FACULTY' ||
     enrollments.some(
       (enrollment: any) =>
         enrollment &&
         enrollment.user === currentUser._id &&
         enrollment.course === cid
-    ) ? (
-    children
-  ) : (
-    <Navigate to="/Kanbas/Dashboard" />
-  );
+    )
+  ) {
+    return children;
+  } else {
+    return <Navigate to="/Kanbas/Dashboard" />;
+  }
 }
