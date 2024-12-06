b3b21d7f7d3543e25126a91e316d4456df69e273
Jacob Kline <jacobk513@gmail.com>
2024-12-06 12:31:12 -0500

    3.6.3

--- src/Kanbas/Account/Users.tsx
+++ src/Kanbas/Account/Users.tsx
@@ -4,6 +4,29 @@ import PeopleTable from '../Courses/People/Table';
 import * as client from './client';
 export default function Users() {
   const [users, setUsers] = useState<any[]>([]);
+  const [role, setRole] = useState('');
+  const filterUsersByRole = async (role: string) => {
+    setRole(role);
+    if (role) {
+      const users = await client.findUsersByRole(role);
+      setUsers(users);
+    } else {
+      fetchUsers();
+    }
+  };
+
+  const [, setName] = useState('');
+
+  const filterUsersByName = async (name: string) => {
+    setName(name);
+    if (name) {
+      const users = await client.findUsersByPartialName(name);
+      setUsers(users);
+    } else {
+      fetchUsers();
+    }
+  };
+
   const { uid } = useParams();
   const fetchUsers = async () => {
     const users = await client.findAllUsers();
@@ -15,6 +38,24 @@ export default function Users() {
   return (
     <div>
       <h3>Users</h3>
+      <input
+        onChange={(e) => filterUsersByName(e.target.value)}
+        placeholder="Search people"
+        className="form-control float-start w-25 me-2 wd-filter-by-name"
+      />
+
+      <select
+        value={role}
+        onChange={(e) => filterUsersByRole(e.target.value)}
+        className="form-select float-start w-25 wd-select-role"
+      >
+        <option value="">All Roles</option>{' '}
+        <option value="STUDENT">Students</option>
+        <option value="TA">Assistants</option>{' '}
+        <option value="FACULTY">Faculty</option>
+        <option value="ADMIN">Administrators</option>
+      </select>
+
       <PeopleTable users={users} />
     </div>
   );
